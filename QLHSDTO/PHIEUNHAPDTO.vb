5efd5f424e5df5586ac3613fd5e3c452eeb8a3e7
NguyenHuynhSang <16521022@gm.uit.edu.vn>
2018-05-23 12:53:40 +0700

    Them source code 1
    source code them 1

---
+++ QLHSDTO/PHIEUNHAPDTO.vb
@@ -0,0 +1,89 @@
+﻿Public Class PHIEUNHAPDTO
+
+    Private maphieunhap As String
+    Private ngaynhapsach As DateTime
+    Private soluongnhap As Int32
+    Private tensach As String
+    Private theloai As String
+    Private tacgia As String
+    Private v1 As Object
+    Private v2 As Object
+    Private v3 As Object
+    Private v4 As Object
+    Private v5 As Object
+    Private v6 As Object
+
+    Public Sub New()
+    End Sub
+
+    Public Sub New(v1 As Object, v2 As Object, v3 As Object, v4 As Object, v5 As Object, v6 As Object)
+        Me.v1 = v1
+        Me.v2 = v2
+        Me.v3 = v3
+        Me.v4 = v4
+        Me.v5 = v5
+        Me.v6 = v6
+    End Sub
+
+    Public Sub New(maPN As String, ngaynhapsach As DateTime, soluongnhap As Int32, tensach As String, theloai As String, tacgia As String)
+        Me.Maphieunhap1 = maPN
+        Me.Ngaynhapsach1 = ngaynhapsach
+        Me.Soluongnhap1 = soluongnhap
+        Me.Tensach1 = tensach
+        Me.Theloai1 = theloai
+        Me.Tacgia1 = tacgia
+    End Sub
+    Public Property Maphieunhap1 As String
+        Get
+            Return maphieunhap
+        End Get
+        Set(value As String)
+            maphieunhap = value
+        End Set
+    End Property
+
+    Public Property Ngaynhapsach1 As Date
+        Get
+            Return ngaynhapsach
+        End Get
+        Set(value As Date)
+            ngaynhapsach = value
+        End Set
+    End Property
+
+    Public Property Soluongnhap1 As Integer
+        Get
+            Return soluongnhap
+        End Get
+        Set(value As Integer)
+            soluongnhap = value
+        End Set
+    End Property
+
+    Public Property Tensach1 As String
+        Get
+            Return tensach
+        End Get
+        Set(value As String)
+            tensach = value
+        End Set
+    End Property
+
+    Public Property Theloai1 As String
+        Get
+            Return theloai
+        End Get
+        Set(value As String)
+            theloai = value
+        End Set
+    End Property
+
+    Public Property Tacgia1 As String
+        Get
+            Return tacgia
+        End Get
+        Set(value As String)
+            tacgia = value
+        End Set
+    End Property
+End Class
---
+++ createQLNS.sql
@@ -0,0 +1,72 @@
+﻿/****** To insert Vietnames:  ******/
+/****** 1. make sure script in Unicode-16 ******/
+/****** 2. Put N before Vietnames text ******/
+/******    Example: N'Nguyễn Công Hoan' ******/
+
+--tạo database
+USE [master]
+GO
+
+WHILE EXISTS(select NULL from sys.databases where name='QLNS')
+BEGIN
+    DECLARE @SQL varchar(max)
+    SELECT @SQL = COALESCE(@SQL,'') + 'Kill ' + Convert(varchar, SPId) + ';'
+    FROM MASTER..SysProcesses
+    WHERE DBId = DB_ID(N'QLNS') AND SPId <> @@SPId
+    EXEC(@SQL)
+    DROP DATABASE [QLNS]
+END
+GO
+
+/* Collation = SQL_Latin1_General_CP1_CI_AS */
+CREATE DATABASE [QLNS]
+GO
+
+--Tạo bảng tblDocGia
+USE [QLNS]
+GO
+
+/****** Object:  Table [dbo].[tblPhieuNhapSach]    Script Date: 05/06/18 5:10:50 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblPhieuNhapSach](
+	[MaPhieuNhap] [int] NOT NULL,
+	[NgayNhapSach] [datetime2](7) NULL,
+	[SoLuongNhap] [int] NOT NULL,
+	[TenSach] [nvarchar](50) NULL,
+	[TheLoai] [nvarchar](50) NULL,
+	[TacGia]  [nvarchar](50) NULL,
+ CONSTRAINT [PK_tblPhieuNhapSach] PRIMARY KEY CLUSTERED 
+(
+	[MaPhieuNhap] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+
+--Insert vào bảng tblPhieuNhapSach
+USE [QLNS]
+GO
+
+INSERT INTO [dbo].[tblPhieuNhapSach]
+           ([MaPhieuNhap]
+           ,[NgayNhapSach]
+           ,[SoLuongNhap]
+           ,[TenSach]
+           ,[TheLoai]
+           ,[TacGia] )
+     VALUES
+           ('1'
+           ,convert(datetime,'01/01/1999 00:00:00',101)
+           ,'1'
+           ,N'twilight'
+           ,N'X'
+           ,N'Nguyễn Van A')
+GO
+
